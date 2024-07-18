7fe34a7b05eab4adbb33cf682f3abbc5c9564c24
L IN <linzb93@users.noreply.github.com>
2024-07-18 22:09:56 +0800

    types:添加导出类型

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,7 @@
 import Application from "./application";
 export { default as Route } from "./Route";
 export { default as request } from "./request";
+export type { MiddlewareContext } from "./types";
 
 export default {
   create() {
